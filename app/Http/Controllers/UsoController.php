0d0db7b7ffcef900c186682207390e6695bf66b0
Lucas <lucashen@gmail.com>
2019-02-04 09:16:49 -0200

    fix uso functions

--- app/Http/Controllers/UsoController.php
+++ app/Http/Controllers/UsoController.php
@@ -9,13 +9,32 @@ use Symfony\Component\HttpFoundation\Response;
 class UsoController extends Controller
 {
     public function get(Request $request) {
-        return DB::select("SELECT * FROM `condicoes_uso` WHERE `active` = 1");
+        $arr = array();
+        $arrItens = array();
+        $uso = DB::select("SELECT * FROM `condicoes_uso` WHERE `active` = 1");
+
+        for ($i = 0; $i < count($uso); $i++) {
+            $opcoes = DB::select("SELECT * FROM `condicoes_uso_opcoes` WHERE `id_condicao_uso` = ? AND `active` = 1", [$uso[$i]->id]);
+            $arr['id'] = $uso[$i]->id;
+            $arr['pergunta'] = $uso[$i]->pergunta;
+            $arr['opcoes'] = $opcoes;
+            array_push($arrItens, $arr);
+        }
+
+        return $arrItens;
     }
 
     public function save(Request $request) {
         try {
             DB::insert('INSERT INTO `condicoes_uso` (`pergunta`) VALUES (?)', [$request->pergunta]);
-            $list = DB::select("SELECT * FROM `condicoes_uso` WHERE `active` = 1");
+            $id = DB::getPdo()->lastInsertId();
+            
+            for ($i = 0; $i < count($request->opcoes); $i++) {
+                DB::insert('INSERT INTO `condicoes_uso_opcoes` (`opcao`, `id_condicao_uso`) VALUES (?, ?)', 
+                    [$request->opcoes[$i], $id]);
+            }
+            
+            $list = $this->get($request);
             $message = 'Condições de uso inserido com sucesso.';
             return $this->successResponse($list, $message);
         } catch (Exception $e) {
@@ -39,7 +58,9 @@ class UsoController extends Controller
     public function remove(Request $request, $id) {
         try {
             DB::update('UPDATE condicoes_uso SET `active` = ? WHERE id = ?', [0, $id]);
-            $list = DB::select("SELECT * FROM `condicoes_uso` WHERE `active` = 1");
+            DB::update('UPDATE condicoes_uso_opcoes SET `active` = ? WHERE id_condicao_uso = ?', [0, $id]);
+
+            $list = $this->get($request);
             $message = 'Condição de uso deletado com sucesso.';
             return $this->successResponse($list, $message);
         } catch (Exception $e) {
